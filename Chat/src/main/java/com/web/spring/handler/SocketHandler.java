189fc302cc8b14dee3dee5275d2965c348118351
kwonseeun <0115km@naver.com>
2022-03-10 17:17:00 +0900

    나, 상대방 구분하기 채팅방 만들기 로직 추가

--- Chat/src/main/java/com/web/spring/handler/SocketHandler.java
+++ Chat/src/main/java/com/web/spring/handler/SocketHandler.java
@@ -2,6 +2,9 @@ package com.web.spring.handler;
 
 import java.util.HashMap;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -11,27 +14,33 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class SocketHandler extends TextWebSocketHandler {
 	
-	HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 맵
+HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 맵
 	
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
 		//메시지 발송
 		String msg = message.getPayload();
+		JSONObject obj = jsonToObjectParser(msg);
 		for(String key : sessionMap.keySet()) {
 			WebSocketSession wss = sessionMap.get(key);
 			try {
-				wss.sendMessage(new TextMessage(msg));
+				wss.sendMessage(new TextMessage(obj.toJSONString()));
 			}catch(Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		//소켓 연결
 		super.afterConnectionEstablished(session);
 		sessionMap.put(session.getId(), session);
+		JSONObject obj = new JSONObject();
+		obj.put("type", "getId");
+		obj.put("sessionId", session.getId());
+		session.sendMessage(new TextMessage(obj.toJSONString()));
 	}
 	
 	@Override
@@ -39,5 +48,16 @@ public class SocketHandler extends TextWebSocketHandler {
 		//소켓 종료
 		sessionMap.remove(session.getId());
 		super.afterConnectionClosed(session, status);
+	}
+	
+	private static JSONObject jsonToObjectParser(String jsonStr) {
+		JSONParser parser = new JSONParser();
+		JSONObject obj = null;
+		try {
+			obj = (JSONObject) parser.parse(jsonStr);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return obj;
 	}
 }
